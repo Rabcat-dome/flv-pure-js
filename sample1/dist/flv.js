dfa7a4f562c6296045948ab467924b989670e743
VISARUTP <visarut.p@pttdigital.com>
2023-03-03 09:48:12 +0700

    upload dist folder

---
+++ sample1/dist/flv.js
@@ -0,0 +1,37 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["flvjs"] = factory();
+	else
+		root["flvjs"] = factory();
+})(self, function() {
+return /******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: Debug Failure. False expression: Non-string value passed to `ts.resolveTypeReferenceDirective`, likely by a wrapping package working with an outdated `resolveTypeReferenceDirectives` signature. This is probably not a problem in TS itself.\n    at Object.resolveTypeReferenceDirective (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\typescript\\lib\\typescript.js:43500:18)\n    at D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\ts-loader\\dist\\servicesHost.js:713:18\n    at D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\ts-loader\\dist\\servicesHost.js:128:141\n    at Array.map (<anonymous>)\n    at Object.resolveTypeReferenceDirectives (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\ts-loader\\dist\\servicesHost.js:128:124)\n    at actualResolveTypeReferenceDirectiveNamesWorker (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\typescript\\lib\\typescript.js:119382:163)\n    at resolveTypeReferenceDirectiveNamesWorker (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\typescript\\lib\\typescript.js:119682:26)\n    at processTypeReferenceDirectives (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\typescript\\lib\\typescript.js:121185:31)\n    at findSourceFileWorker (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\typescript\\lib\\typescript.js:121070:21)\n    at findSourceFile (D:\\march\\Downloads\\flv.js-master\\flv.js-master\\node_modules\\typescript\\lib\\typescript.js:120922:26)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
+//# sourceMappingURL=flv.js.map
